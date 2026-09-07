77da3d6a61413e23d283a50938ca20d2bc62c25f
Alfahaaan <mohamad.ali@brexa.com>
2026-09-07 15:21:05 +0700

    feat: setup-middleware

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::prefix("dashboard")
+    ->middleware(["auth:api", "admin"])
+    ->group(function () {});
+
+Route::prefix("mobile")
+    ->middleware(["auth:api", "user"])
+    ->group(function () {});
